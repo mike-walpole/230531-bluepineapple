52b30f23c782d62d9f0bfa8c23697b4e9f5d0057
mike-walpole <walpole.mike@gmail.com>
2023-05-31 13:04:08 +0200

    new testimonial

--- src/components/policy.svelte
+++ src/components/policy.svelte
@@ -5,29 +5,60 @@
 <div id="section-2" class="snap-start grid grid-cols-1 md:grid-cols-3 md:h-screen snap-center">
 	<div
 		id="container for logos"
-		class="grid grid-cols-1 md:grid-cols-3 col-start-1 col-end-3 md:my-auto order-last md:order-first grayscale"
+		class="grid grid-cols-1 md:grid-cols-2 col-start-1 col-end-3 md:my-auto order-last md:order-first grayscale"
 	>
-		<div class="{width} mx-auto  my-2 mb-5 md:mb-0 border rounded shadow-lg px-8 pt-8 pb-5">
-			<p class="mb-5 ">
-				What I really like is their flexibility especially when we ran into unexcpected issues. They
-				were meticulous in following up on feedback.
-			</p>
-			<p class="font-bold">Confidential, US</p>
-		</div>
-		<div class="{width} mx-auto  my-2 mb-5 md:mb-0 border rounded shadow-lg px-8 pt-8 pb-5">
-			<p class="mb-5">
-				Very proactive and responsive. Always willing to go the extra mile to support the customer
-			</p>
-			<p class="mb-5 font-bold">SLE Worldwide</p>
-		</div>
-		<div class="{width} mx-auto  my-2 mb-5 md:mb-0 border rounded shadow-lg px-8 pt-8 pb-5">
-			<p class="mb-5">
-				Blupineapple has been a trusted partner for us. They are very professional, taking a
-				consultative approach to all development requests. We look forward to our continued
-				partnership with Bluepineapple.
-			</p>
-			<p class="mb-5 font-bold">BitSight Technologies</p>
-		</div>
+		<figure class="max-w-screen-md px-5 mx-auto text-center">
+			<svg
+				aria-hidden="true"
+				class="w-12 h-12 mx-auto mb-3 text-gray-400"
+				viewBox="0 0 24 27"
+				fill="none"
+				xmlns="http://www.w3.org/2000/svg"
+				><path
+					d="M14.017 18L14.017 10.609C14.017 4.905 17.748 1.039 23 0L23.995 2.151C21.563 3.068 20 5.789 20 8H24V18H14.017ZM0 18V10.609C0 4.905 3.748 1.038 9 0L9.996 2.151C7.563 3.068 6 5.789 6 8H9.983L9.983 18L0 18Z"
+					fill="currentColor"
+				/></svg
+			>
+			<blockquote>
+				<p class="text-2xl italic font-medium text-gray-900 ">
+					"Bluepineapple has been a trusted partner for us. They are very professional, taking a
+					consultative approach to all development requests. We look forward to our continued
+					partnership with Bluepineapple."
+				</p>
+			</blockquote>
+			<figcaption class="flex items-center justify-center mt-6 space-x-3">
+				<div class="flex items-center divide-x-2 divide-gray-500 ">
+					<cite class="pr-3 font-medium text-gray-900 ">BitSight Technologies</cite>
+					<cite class="pl-3 text-sm text-gray-500 ">US</cite>
+				</div>
+			</figcaption>
+		</figure>
+
+		<figure class="max-w-screen-md mx-auto text-center px-5">
+			<svg
+				aria-hidden="true"
+				class="w-12 h-12 mx-auto mb-3 text-gray-400"
+				viewBox="0 0 24 27"
+				fill="none"
+				xmlns="http://www.w3.org/2000/svg"
+				><path
+					d="M14.017 18L14.017 10.609C14.017 4.905 17.748 1.039 23 0L23.995 2.151C21.563 3.068 20 5.789 20 8H24V18H14.017ZM0 18V10.609C0 4.905 3.748 1.038 9 0L9.996 2.151C7.563 3.068 6 5.789 6 8H9.983L9.983 18L0 18Z"
+					fill="currentColor"
+				/></svg
+			>
+			<blockquote>
+				<p class="text-2xl italic font-medium text-gray-900 ">
+					"What I really like is their flexibility especially when we ran into unexcpected issues.
+					They were meticulous in following up on feedback."
+				</p>
+			</blockquote>
+			<figcaption class="flex items-center justify-center mt-6 space-x-3">
+				<div class="flex items-center divide-x-2 divide-gray-500 ">
+					<cite class="pr-3 font-medium text-gray-900 ">Confidential client</cite>
+					<cite class="pl-3 text-sm text-gray-500 ">US</cite>
+				</div>
+			</figcaption>
+		</figure>
 	</div>
 
 	<div id="container for words" class="pt-10 col-span-1 px-5 pr-10 mb-5 md:mb-0">
